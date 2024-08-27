d06838169fd83554feae28c7e30e1f645fc0c45f
Rafa Lopes <rafalopessecond@gmail.com>
2024-08-27 14:01:01 +0100

    update about

---
+++ src/pages/About.tsx
@@ -0,0 +1,27 @@
+export default function About() {
+	return (
+		<>
+			<h2 className="title">About Me</h2>
+
+			<p>
+				Hi there, I'm Rafa! I'm an enthusiastic full-stack software
+				developer with experience in building applications with
+				real-world use and leveraging the benefits of agile development
+				environments.
+			</p>
+			<p>
+				In my free time I like to tend to my homestead garden, ride
+				motorcycles and travel!
+			</p>
+			<p>
+				Check out my projects and{" "}
+				<a
+					href="https://www.linkedin.com/in/rafael-lopes-software-developer/"
+					target="_blank">
+					LinkedIn
+				</a>{" "}
+				to know more!
+			</p>
+		</>
+	);
+}
