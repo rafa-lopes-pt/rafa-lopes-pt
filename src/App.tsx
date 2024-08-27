cd04893cefb6c2a6b1a2f3ae82714c9321b5861a
Rafa Lopes <rafalopessecond@gmail.com>
2024-08-27 10:50:52 +0100

    add initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useRef } from "react";
+import Background from "./components/misc/Background";
+import ScrollSection from "./components/containers/ScrollSection";
+
 function App() {
-	return <></>;
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	return (
+		<>
+			<Background containerRef={containerRef} />
+
+			<div
+				className="scrollable"
+				ref={containerRef}>
+				<ScrollSection className="hero">
+					<h1>Hero</h1>
+					<h1>About</h1>
+					<h1>TechStack</h1>
+					<h1>Prev work</h1>
+					<h1>Contacts</h1>
+				</ScrollSection>
+			</div>
+		</>
+	);
 }
 
 export default App;
